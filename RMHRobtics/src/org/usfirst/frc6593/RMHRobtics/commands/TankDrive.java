b93de7b02633f829919fae164742049e321e4c31
Admin <Admin@LR90KDC5K>
2017-02-20 16:20:42 -0500

    # WARNING: head commit changed in the meantime
    gyro

--- RMHRobtics/src/org/usfirst/frc6593/RMHRobtics/commands/TankDrive.java
+++ RMHRobtics/src/org/usfirst/frc6593/RMHRobtics/commands/TankDrive.java
@@ -37,6 +37,8 @@ public class TankDrive extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	
+    	Robot.driveTrain.GyroReset();
     }
 
     // Called repeatedly when this Command is scheduled to run
