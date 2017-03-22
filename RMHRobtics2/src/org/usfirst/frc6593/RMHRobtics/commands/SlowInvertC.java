f71602654796a751ec0d082b988ad7a50bee502b
Admin <Admin@LR90KDC5K.frc-robot.local>
2017-03-22 17:12:38 -0400

    added slow climbing button

--- RMHRobtics2/src/org/usfirst/frc6593/RMHRobtics/commands/SlowInvertC.java
+++ RMHRobtics2/src/org/usfirst/frc6593/RMHRobtics/commands/SlowInvertC.java
@@ -7,22 +7,23 @@ import edu.wpi.first.wpilibj.command.Command;
 /**
  *
  */
-public class InvertClimbing extends Command {
+public class SlowInvertC extends Command {
 
-
-    public InvertClimbing() {
+    public SlowInvertC() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
     	requires(Robot.climbing);
-    	
+
     }
 
-	// Called just before this Command runs the first time
+    // Called just before this Command runs the first time
     protected void initialize() {
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	
-		Robot.climbing.invertRolling(1);
+		Robot.climbing.invertRolling(.5);
     }
 
     // Make this return true when this Command no longer needs to run execute()
